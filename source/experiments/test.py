3f3328ea8ba29eab8329be05c2256049106feb50
Maximilian <maximilian.arling@stud.uni-hannover.de>
2025-11-03 16:10:50 +0100

    Made vec envs picklable

--- source/experiments/test.py
+++ source/experiments/test.py
@@ -18,29 +18,25 @@ def main(cfg: DictConfig) -> float:
     print(f"Environment Obs: {env.observation_space}")
     print(f"Environment Act: {env.action_space}")
 
-    model = PPO(
-        "MlpPolicy",
-        env,
-        device="cpu",
-    )
+    model = PPO("MlpPolicy", env, device="cpu", verbose=1)
 
-    model.learn(total_timesteps=int(100000), progress_bar=True)
+    model.learn(total_timesteps=int(1000000), progress_bar=True)
 
     mean_reward, std_reward = evaluate_policy(
         model, model.get_env(), n_eval_episodes=10
     )
     print(f"mean reward: {mean_reward}, std reward: {std_reward}")
 
-    # while True:
-    #     # deterministic=True für stabilere Demo
-    #     action = env.action_space.sample()
-    #     obs, _, terminated, truncated, _ = env.step(action)
+    while True:
+        # deterministic=True für stabilere Demo
+        action = env.action_space.sample()
+        obs, _, terminated, truncated, _ = env.step(action)
 
-    #     # Render ruft der Env-Treiber selbst auf, aber explizit geht auch:
-    #     env.unwrapped.mj_render()
+        # Render ruft der Env-Treiber selbst auf, aber explizit geht auch:
+        env.unwrapped.mj_render()
 
-    #     if terminated or truncated:
-    #         obs, _ = env.reset()
+        if terminated or truncated:
+            obs, _ = env.reset()
 
     # return 0.0
 
